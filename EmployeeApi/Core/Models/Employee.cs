372a974b9e8631ebb382c9884d66c94ae4453b18
DavidBewley <davidmartinbewley@gmail.com>
2021-10-14 19:14:50 +0100

    Added controller endpoints

---
+++ EmployeeApi/Core/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Core.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public Employee() { }
+
+        public Employee(Guid id, string name, DateTime startDate)
+        {
+            Id = id;
+            Name = name;
+            StartDate = startDate;
+        }
+    }
+}
---
+++ EmployeeApi/EmployeeApi/Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            return StatusCode(501);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Employee employee)
+        {
+            return StatusCode(501);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] Employee employee)
+        {
+            return StatusCode(501);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return StatusCode(501);
+        }
+    }
+}
